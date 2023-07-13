e4d550d90f2d746dee95e9dceff2070267d5e9b8
Kevin <kevinlioni91@gmail.com>
2023-07-12 21:33:50 -0300

    primer commit

---
+++ js.js
@@ -0,0 +1,41 @@
+
+
+
+function realizarDivision() {
+    var dividendo = document.getElementById("dividendo").value;
+    var divisor = document.getElementById("divisor").value;
+  
+    if (divisor === '0') {
+      document.getElementById("resultado").innerHTML = "El divisor no puede ser cero";
+    } else {
+      var resultado = dividendo / divisor;
+      document.getElementById("resultado").innerHTML = "El resultado de la división es: " + resultado;
+    }
+  }
+  function realizarResta() {
+    var minuendo = document.getElementById("minuendo").value;
+    var sustraendo = document.getElementById("sustraendo").value;
+  
+      var resultado = minuendo - sustraendo;
+      document.getElementById("resultado2").innerHTML = "El resultado de la resata es: " + resultado;
+    }
+
+    function realizarSuma() {
+      var sumandoa = document.getElementById("sumandoa").value;
+      var sumandob = document.getElementById("sumandob").value;
+    
+        var resultado = sumandoa + sumandob;
+        document.getElementById("resultado3").innerHTML = "El resultado de la suma es: " + resultado;
+      }
+
+
+
+//var numero1 = parseFloat(prompt("Ingresa el primer número:"));
+//var numero2 = parseFloat(prompt("Ingresa el segundo número:"));
+
+//if (numero2 === 0) {
+//  console.log("El divisor no puede ser cero.");
+//} else {
+//  var resultado = numero1 / numero2;
+//  console.log("El resultado de la división es: " + resultado);
+//}
